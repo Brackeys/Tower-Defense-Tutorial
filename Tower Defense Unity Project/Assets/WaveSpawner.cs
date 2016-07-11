8b47103a0f6b7885b1d60c329200b84f878d98b9
Asbjørn Thirslund <at@brackeys.com>
2016-07-11 13:46:34 +0200

    Video 3: Wave Spawner

---
+++ Tower Defense Unity Project/Assets/WaveSpawner.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+	public Transform enemyPrefab;
+
+	public Transform spawnPoint;
+
+	public float timeBetweenWaves = 5f;
+	private float countdown = 2f;
+
+	public Text waveCountdownText;
+
+	private int waveIndex = 0;
+
+	void Update ()
+	{
+		if (countdown <= 0f)
+		{
+			StartCoroutine(SpawnWave());
+			countdown = timeBetweenWaves;
+		}
+
+		countdown -= Time.deltaTime;
+
+		waveCountdownText.text = Mathf.Round(countdown).ToString();
+	}
+
+	IEnumerator SpawnWave ()
+	{
+		waveIndex++;
+
+		for (int i = 0; i < waveIndex; i++)
+		{
+			SpawnEnemy();
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+
+	void SpawnEnemy ()
+	{
+		Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+
+}
