41eee8d3dfb2148f2944c69b11ff8fa471d889ee
Asbjørn Thirslund <at@brackeys.com>
2016-08-15 13:57:36 +0200

    Video 7: Camera

---
+++ Tower Defense Unity Project/Assets/CameraController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private bool doMovement = true;
+
+	public float panSpeed = 30f;
+	public float panBorderThickness = 10f;
+
+	public float scrollSpeed = 5f;
+	public float minY = 10f;
+	public float maxY = 80f;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+			doMovement = !doMovement;
+
+		if (!doMovement)
+			return;
+
+		if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+		{
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+		{
+			transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+		{
+			transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+		{
+			transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+		}
+
+		float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+		Vector3 pos = transform.position;
+
+		pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+		pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+		transform.position = pos;
+
+	}
+}
