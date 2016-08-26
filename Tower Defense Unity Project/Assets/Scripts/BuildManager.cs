db58f9809b4c075e1352741e84c3f1ee240b1ef9
Asbjørn Thirslund <at@brackeys.com>
2016-08-26 20:30:04 +0200

    Video 9: Missile Launcher

--- Tower Defense Unity Project/Assets/Scripts/BuildManager.cs
+++ Tower Defense Unity Project/Assets/Scripts/BuildManager.cs
@@ -15,7 +15,7 @@ public class BuildManager : MonoBehaviour {
 	}
 
 	public GameObject standardTurretPrefab;
-	public GameObject anotherTurretPrefab;
+	public GameObject missileLauncherPrefab;
 
 	private GameObject turretToBuild;
 
--- Tower Defense Unity Project/Assets/Scripts/Shop.cs
+++ Tower Defense Unity Project/Assets/Scripts/Shop.cs
@@ -15,10 +15,10 @@ public class Shop : MonoBehaviour {
 		buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
 	}
 
-	public void PurchaseAnotherTurret()
+	public void PurchaseMissileLauncher()
 	{
-		Debug.Log("Another Turret Selected");
-		buildManager.SetTurretToBuild(buildManager.anotherTurretPrefab);
+		Debug.Log("Missile Launcher Selected");
+		buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab);
 	}
 
 }
