546cb700419e14559c327b6f31b9e7395ebae981
Asbjørn Thirslund <at@brackeys.com>
2016-09-04 20:04:11 +0200

    Video 10: Missile

--- Tower Defense Unity Project/Assets/Scripts/Bullet.cs
+++ Tower Defense Unity Project/Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : MonoBehaviour {
 	private Transform target;
 
 	public float speed = 70f;
+	public float explosionRadius = 0f;
 	public GameObject impactEffect;
 	
 	public void Seek (Transform _target)
@@ -31,15 +32,46 @@ public class Bullet : MonoBehaviour {
 		}
 
 		transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+		transform.LookAt(target);
 
 	}
 
 	void HitTarget ()
 	{
 		GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
-		Destroy(effectIns, 2f);
+		Destroy(effectIns, 5f);
 
-		Destroy(target.gameObject);
+		if (explosionRadius > 0f)
+		{
+			Explode();
+		} else
+		{
+			Damage(target);
+		}
+
 		Destroy(gameObject);
+	}
+
+	void Explode ()
+	{
+		Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+		foreach (Collider collider in colliders)
+		{
+			if (collider.tag == "Enemy")
+			{
+				Damage(collider.transform);
+			}
+		}
+	}
+
+	void Damage (Transform enemy)
+	{
+		Destroy(enemy.gameObject);
+	}
+
+	void OnDrawGizmosSelected ()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, explosionRadius);
 	}
 }
