f55c95f41f9a9adb78e1ebda3fe79e3612d671ea
Asbjørn Thirslund <at@brackeys.com>
2016-11-21 15:28:23 +0100

    Video 23: Health Bars

--- Tower Defense Unity Project/Assets/Scripts/Enemy.cs
+++ Tower Defense Unity Project/Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
@@ -7,20 +8,27 @@ public class Enemy : MonoBehaviour {
 	[HideInInspector]
 	public float speed;
 
-	public float health = 100;
+	public float startHealth = 100;
+	private float health;
 
 	public int worth = 50;
 
 	public GameObject deathEffect;
 
+	[Header("Unity Stuff")]
+	public Image healthBar;
+
 	void Start ()
 	{
 		speed = startSpeed;
+		health = startHealth;
 	}
 
 	public void TakeDamage (float amount)
 	{
 		health -= amount;
+
+		healthBar.fillAmount = health / startHealth;
 
 		if (health <= 0)
 		{
